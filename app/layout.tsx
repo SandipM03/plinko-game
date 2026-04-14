4aa56d3236a0aa7fecf948e4ac91269a957beeff
SandipM03 <sandipmandal816708@gmail.com>
2026-04-15 03:20:18 +0530

    add prisma schema

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter, Geist } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+export const metadata: Metadata = {
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={cn("dark", "h-full", "antialiased", "font-sans", geist.variable)}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
